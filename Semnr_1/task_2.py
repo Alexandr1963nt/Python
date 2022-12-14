8f75bd525cb6163e359c52dfa0aa02e918cbf23b
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2022-12-14 21:14:04 +0300

    В task_2 добавлено решение рекурсией

--- Semnr_1/task_2.py
+++ Semnr_1/task_2.py
@@ -1,12 +1,18 @@
 # Задача 2
 # Найдите сумму цифр трехзначного числа.
 # Решение годится для числа любой разрядности
-input_number = int(input('\nВведите целое число -> '))
-if input_number < 0:    number = - input_number
-else:    number = input_number
-sum_number_digitals = 0
+input_number = int(input('\nВведите целое число -> '))  # Общая часть
+if input_number < 0:    number = - input_number         # Общая часть
+else:    number = input_number                          # Общая часть
+# Первый вариант решения через рекурсию
+def sum_number_digitals (number):
+    if (number // 10 == 0) : return number % 10
+    else : return sum_number_digitals (number//10) + number % 10
+print(f'\nСумма цифр числа {input_number} рекурсией равна {sum_number_digitals(number)}')
+# Второй вариант решения через while
+sum_number_digit_while = 0
 while number > 0:
     digital = number % 10
-    sum_number_digitals += digital
+    sum_number_digit_while += digital
     number //= 10
-print(f'\nСумма цифр числа {input_number} равна {sum_number_digitals}\n')
+print(f'\nСумма цифр числа {input_number} циклом while равна {sum_number_digit_while}\n')   

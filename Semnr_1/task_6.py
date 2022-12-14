ea14cdf6f0eb3e07d4d87276cee69b43e224dc5f
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2022-12-14 22:30:38 +0300

    add решение для типа int в task_6

--- Semnr_1/task_6.py
+++ Semnr_1/task_6.py
@@ -9,23 +9,32 @@
 # 385916 -> yes
 # 123456 -> no
 
-def SumElements(n, m):
-    sum = 0
-    for i in range(n, m):
-        sum += int(ticket_number[i])
-    return sum
+def SumElements(n, m):                    # - 1 вариант решения для str
+    sum = 0                               # - 1 вариант решения для str
+    for i in range(n, m):                 # - 1 вариант решения для str
+        sum += int(ticket_number[i])      # - 1 вариант решения для str
+    return sum                            # - 1 вариант решения для str
+
+# def sum_number_digitals (number):                                   # 2 вариант для int
+#     if (number // 10 == 0) : return number % 10                     # 2 вариант для int
+#     else : return sum_number_digitals (number//10) + number % 10    # 2 вариант для int
 
 are_you_happy = False
 while are_you_happy == False:
     ticket_number = input('\nВведите № билета Например: \n385916 - счастливый\nили \n123456 - нет --> ')
     while len(ticket_number) % 2 != 0:
         ticket_number = input('\nВведите число с четным количеством цифр--> ')
-    sum1 = SumElements(0, len(ticket_number)//2)
-    sum2 = SumElements(len(ticket_number)//2, len(ticket_number) )
+    sum1 = SumElements(0, len(ticket_number)//2)                  # вычслить сумму цифр строки
+    sum2 = SumElements(len(ticket_number)//2, len(ticket_number)) # по индексам в строке
+    
+    # ticket_num_int = int(ticket_number)                 # для варианта с int
+    # sum1 = sum_number_digitals (ticket_num_int // 1000) # для варианта с int
+    # sum2 = sum_number_digitals (ticket_num_int % 1000)  # для варианта с int
+
     if sum1 == sum2:
         are_you_happy = True
         print('\nСупер! Вам достался счастивый билет!' +
-            f' {ticket_number[:len(ticket_number)//2]} => {sum1}.' +
+            f' {ticket_number[:len(ticket_number)//2]} => {sum1}. ' +
             f' {ticket_number[len(ticket_number)//2:len(ticket_number)]} => {sum2}.' +
             ' Приятного аппетита! :)\n')
     else:

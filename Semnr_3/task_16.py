32328857bdcef07f29e1fe16702001318b3f61b7
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2022-12-22 02:47:07 +0300

    Измнил решение task_20

--- Semnr_3/task_16.py
+++ Semnr_3/task_16.py
@@ -12,6 +12,7 @@
 # Вывод: 1
 
 import os
+import time
 
 
 def create_array(N):
@@ -29,16 +30,69 @@ def ending_word_times(n):
         return 'раз'
 
 
+# унарный поиск вхождения искомого элемента в массив
+def count_element_in_array(arr, element):
+    count = 0
+    for i in range(0, len(arr)):
+        if arr[i] == element:
+            count += 1
+    return count
+
+# Бинарныый поиск - прикольная штука - ооооочень быыыыстрый !!!!!!
+
+
+def binary_search(array, item):
+    if len(array) < 2 and array[0] == item: return 1
+    elif len(array) < 2 and array[0] != item : return 0
+    else:
+        low = 0
+        high = len(array) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            guess = array[mid]
+            if guess == item:
+                array.pop(mid)
+                return 1 + binary_search(array, item)
+            if guess > item:
+                high = mid - 1
+            else:
+                low = mid + 1
+    return 0
+
+
 os.system('cls')
 
 N = int(input('Введите количество элементов в массиве N : '))
 user_list = create_array(N)
-print(f'Сформирован {user_list}')
+# print(f'Сформирован {user_list}')
 X = int(input('Введите число, которое необходимо проверить в массиве - X : '))
+
+start1 = time.time_ns()
 how_many_time_was_X_found = user_list.count(X)
+end1 = (time.time_ns()-start1)/1000000
+print('Поиск count(X) = ', end1, 'ms')
+
+start = time.time_ns()
+how_many_time_was_X_found = count_element_in_array(user_list, X)
+end = (time.time_ns()-start)/1000000
+print('Поиск в цикле = ', end, 'ms')
+
+start2 = time.time_ns()
+user_list.sort()
+# print(user_list)
+end2 = (time.time_ns()-start2)/1000000
+print('array.sort() = ', end2, 'ms')
+
+start3 = time.time_ns()
+fast = binary_search(user_list, X)
+print('fast = ', fast, 'раз' )
+end3 = (time.time_ns()-start3)/1000000
+print('binary_search = ', end3, 'ms')
+
 if how_many_time_was_X_found > 0:
     print(
         f'Число {X} встречается в массиве {how_many_time_was_X_found} '
         f'{ending_word_times (how_many_time_was_X_found)}\n')
 else:
     print(f'Числа {X} нет в массиве\n')
+
--- Semnr_3/task_20.py
+++ Semnr_3/task_20.py
@@ -1,19 +1,72 @@
-# Задача 20:
-# В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
-# Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# # Задача 20:
+# # В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+# # Напишите программу, которая вычисляет стоимость введенного пользователем слова.
 
-# Ввод: ноутбук
-# Вывод: 12
-
+# # Ввод: ноутбук
+# # Вывод: 12
+# dictnry = {
+#     'A': 1,
+#     'E': 1,
+#     'I': 1,
+#     'O': 1,
+#     'U': 1,
+#     'L': 1,
+#     'N': 1,
+#     'S': 1,
+#     'T': 1,
+#     'R': 1,
+#     'D': 2,
+#     'G': 2,
+#     'B': 3,
+#     'C': 3,
+#     'M': 3,
+#     'P': 3,
+#     'F': 4,
+#     'H': 4,
+#     'V': 4,
+#     'W': 4,
+#     'Y': 4,
+#     'K': 5,
+#     'J': 8,
+#     'X': 8,
+#     'Q': 10,
+#     'Z': 10,
+#     'А': 1,
+#     'В': 1,
+#     'Е': 1,
+#     'И': 1,
+#     'Н': 1,
+#     'О': 1,
+#     'Р': 1,
+#     'С': 1,
+#     'Т': 1,
+#     'Д': 2,
+#     'К': 2,
+#     'Л': 2,
+#     'М': 2,
+#     'П': 2,
+#     'У': 2,
+#     'Б': 3,
+#     'Г': 3,
+#     'Ё': 3,
+#     'Ь': 3,
+#     'Я': 3,
+#     'Й': 4,
+#     'Ы': 4,
+#     'Ж': 5,
+#     'З': 5,
+#     'Х': 5,
+#     'Ц': 5,
+#     'Ч': 5,
+#     'Ш': 8,
+#     'Э': 8,
+#     'Ю': 8,
+#     'Ф': 10,
+#     'Щ': 10,
+#     'Ъ': 10,
+# }
+# Задача 20: по настольной игре Скрабл (Scrabble)
 import os
-
-# def scrabble(dictionary, word):
-#     keys = word.upper()
-#     word_cost = 0
-#     for i in keys:
-#         word_cost += dictionary[i]
-#     return word_cost
-
 def scrabble(dictionary, word):
     keys = word.upper()
     word_cost = 0
@@ -21,83 +74,57 @@ def scrabble(dictionary, word):
         word_cost += dictionary.get(i, 0)
     return word_cost
 
-dictnry = {
-    'A': 1,
-    'E': 1,
-    'I': 1,
-    'O': 1,
-    'U': 1,
-    'L': 1,
-    'N': 1,
-    'S': 1,
-    'T': 1,
-    'R': 1,
-    'D': 2,
-    'G': 2,
-    'B': 3,
-    'C': 3,
-    'M': 3,
-    'P': 3,
-    'F': 4,
-    'H': 4,
-    'V': 4,
-    'W': 4,
-    'Y': 4,
-    'K': 5,
-    'J': 8,
-    'X': 8,
-    'Q': 10,
-    'Z': 10,
-    'А': 1,
-    'В': 1,
-    'Е': 1,
-    'И': 1,
-    'Н': 1,
-    'О': 1,
-    'Р': 1,
-    'С': 1,
-    'Т': 1,
-    'Д': 2,
-    'К': 2,
-    'Л': 2,
-    'М': 2,
-    'П': 2,
-    'У': 2,
-    'Б': 3,
-    'Г': 3,
-    'Ё': 3,
-    'Ь': 3,
-    'Я': 3,
-    'Й': 4,
-    'Ы': 4,
-    'Ж': 5,
-    'З': 5,
-    'Х': 5,
-    'Ц': 5,
-    'Ч': 5,
-    'Ш': 8,
-    'Э': 8,
-    'Ю': 8,
-    'Ф': 10,
-    'Щ': 10,
-    'Ъ': 10,
-}
+def create_dictionary(language, points):
+    dictionary = dict()
+    for k, v in zip(language, points):
+        dop = dict.fromkeys(k, v)
+        # print(dop)
+        dictionary.update(dop)
+        # print(dictionary)
+    return dictionary
+
+def identify_language(string):
+    if set(string).issubset(set(english)):
+        return (eng, string)
+    elif set(string).issubset(set(russian)):
+        return (rus, string)
+    else:
+        print('уууппссс.. Некорректный ввод! попробуйте ещё раз')
+        word = input('Введите слово повесомей на русском или английском :)) ')
+        return (False, word)
+
+e1 = 'AEIOULNSTR'   # - 1 очко;
+e2 = 'DG'           # - 2 очка;
+e3 = 'BCMP'         # - 3 очка;
+e4 = 'FHVWY'        # - 4 очка;
+e5 = 'K'            # - 5 очков;
+e8 = 'JX'           # - 8 очков;
+e10 = 'QZ'          # - 10 очков.
+
+r1 = 'АВЕИНОРСТ'    # – 1 очко;
+r2 = 'ДКЛМПУ'       # – 2 очка;
+r3 = 'БГЁЬЯ'        # – 3 очка;
+r4 = 'ЙЫ'           # – 4 очка;
+r5 = 'ЖЗХЦЧ'        # – 5 очков;
+r8 = 'ШЭЮ'          # – 8 очков;
+r10 = 'ФЩЪ'         # – 10 очков.
+russian = r1 + r2 + r3+r4+r5+r8+r10
+english = e1 + e2 + e3+e4+e5+e8+e10
+eng = (e1, e2, e3, e4, e5, e8, e10)
+rus = (r1, r2, r3, r4, r5, r8, r10)
+points_set = (1, 2, 3, 4, 5, 8, 10)
 
 os.system('cls')
 
-# word = input('Введите слово на русском или английском : ')
-# try:
-#     print(f'Весомый аргумент! Целых {scrabble (dictnry, word)} баллов\n')
-# except:
-#     print('Неверный ввод!')
-
-# from langdetect import detect
 word = input('Введите слово на русском или английском : ')
-# language = detect(word)
 
-# while 'en' != language != 'ru': 
-#     print(f'уууппссс.. Google считает язык ввода - "{language}" ') 
-#     word = input('Введите слово повесомей на русском или английском :)) ')
-#     language = detect(word)
+language = False
+result = (language, word)  # кортеж
+
+while result[0] == False:
+    result = identify_language(word.upper())
+    language, word = result
+    
+dictnry = create_dictionary(language, points_set)
 
 print(f'Весомый аргумент! Целых {scrabble (dictnry, word)} баллов\n')
